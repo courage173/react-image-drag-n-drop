14700f14463bc9a30b88a072b33e3606c67f2eda
courage173 <courageosemwengie@gmail.com>
2021-03-06 14:27:28 +0100

    initial project setup

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export interface IAppProps {
+}
+
+class App extends React.Component<IAppProps> {
+  public render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+
+export default App;

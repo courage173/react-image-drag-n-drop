a0443ecd82ab089f04647750f777228fcff45486
courage173 <courageosemwengie@gmail.com>
2021-03-13 12:51:50 +0100

    created the html drag functions to handle image swapping

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,61 @@
 import * as React from 'react';
 
 export interface IAppProps {
+  images: [];
+  styles: {};
+  onDragFinish: any,
+  opacity: number,
 }
 
-class App extends React.Component<IAppProps> {
-  public render() {
-    return (
-      <div>
-        
-      </div>
-    );
+function ReactDrag({ images, styles, onDragFinish, opacity= 0.5 }: IAppProps): JSX.Element {
+  let drageSrcEl = null as any
+  const handleDragStart = (e: any): void => {
+    drageSrcEl = e.target;
+    e.dataTransfer.effectAllowed = "move"
+    e.dataTransfer.setData('text/html', e.target.src);
   }
+  const handleDrageEnter = (e: any): void => {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  const handleDragLeave = (e: any): void => {
+    e.preventDefault();
+    e.stopPropagation();
+    drageSrcEl.style.opacity = opacity
+  }
+  const handleDragOver = (e: any): void => {
+    if(e.preventDefault){
+      e.preventDefault()
+    }
+    e.dataTransfer.dropEffect = "move"
+  }
+  const handleDrop = (e: any): void => {
+    if(e.stopPropagation){
+      e.stopPropagation()
+    }
+    if(drageSrcEl && drageSrcEl.src != e.target.src){
+      const d = drageSrcEl.src 
+      drageSrcEl.src = e.target.src;
+      e.target.src = d;
+      e.target.style.opacity  = 1
+      drageSrcEl.style.opacity = 1
+       onDragFinish('')
+    }
+  }
+  return (
+      <>
+      {images.map((im, id) => (
+        <img src={im} alt="new image" style={styles} draggable={true}
+        key={im+id.toString()}
+        onDragStart={handleDragStart}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        onDragEnter={handleDrageEnter}
+        onDragLeave={handleDragLeave}
+        />
+      ))}
+      </>
+  );
 }
 
-
-export default App;
+export default ReactDrag;
